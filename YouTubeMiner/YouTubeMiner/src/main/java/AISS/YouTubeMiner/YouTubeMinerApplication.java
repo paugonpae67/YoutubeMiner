5da179acb065c6c00beeaa06a527ca58d5d045dc
paugonpae67 <114425589+paugonpae67@users.noreply.github.com>
2024-04-10 11:23:01 +0200

    Add files via upload

---
+++ YouTubeMiner/YouTubeMiner/src/main/java/AISS/YouTubeMiner/YouTubeMinerApplication.java
@@ -0,0 +1,13 @@
+package AISS.YouTubeMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeMinerApplication.class, args);
+	}
+
+}
---
+++ YouTubeMiner/YouTubeMiner/src/test/java/AISS/YouTubeMiner/YouTubeMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package AISS.YouTubeMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
