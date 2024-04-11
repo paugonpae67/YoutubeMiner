640e10ec010d6da339f0fb2a9517de484c18899f
Ivo Raimondi <raimondiivo@gmail.com>
2024-04-11 18:07:44 +0200

    Channel service

--- YouTubeMiner/YouTubeMiner/src/main/java/AISS/YouTubeMiner/service/prueba.java
+++
@@ -1,5 +0,0 @@
-package AISS.YouTubeMiner.service;
-
-public class prueba {
-    //Clase de prueba, borrar
-}
---
+++ YouTubeMiner/YouTubeMiner/src/test/java/AISS/YouTubeMiner/service/ChannelServiceTest.java
@@ -0,0 +1,36 @@
+package AISS.YouTubeMiner.service;
+
+import AISS.YouTubeMiner.model.youtube.channel.Channel;
+import AISS.YouTubeMiner.model.youtube.channel.ChannelSearch;
+import AISS.YouTubeMiner.model.youtube.channel.ChannelSnippet;
+import AISS.YouTubeMiner.model.youtube.videoSnippet.VideoSnippet;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ChannelServiceTest {
+
+    @Autowired
+    ChannelService service;
+
+    @Test
+    @DisplayName("Get a channel")
+    void findChannel() {
+        ChannelSnippet channel = service.findChannel("GoogleDevelopers");
+        System.out.println("Funciono + " + channel + " :)");
+    }
+
+    @Test
+    @DisplayName("Get videos from a channel")
+    void findVideosChannel(String name) {
+        List<VideoSnippet> lista = service.findVideosChannel(name);
+        System.out.println("Funciono + " + lista + " ;)");
+    }
+
+
+}
